5afbe18e3b64edf49e12a888e21fb9cdaa4a2b71
subash955 <47134669+subash955@users.noreply.github.com>
2019-10-31 17:05:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -561,7 +561,7 @@ cron.schedule("0 0 0 * *", async function() {
     })
   });
 
-app.listen(8080,process.env.IP, function()
+app.listen(3000,process.env.IP, function()
 {
     console.log('Server Has Started!')
 })
